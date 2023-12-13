e7834f93be3c25e72a9f12197c08ad133931aeb1
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-13 07:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+
+
+let value1=prompt("enter the 1st no.")
+     let result=value1*2
+
+let value2=prompt("enter the 2nd no.")
+
+let result +=parseFloat(value2)
+
+alert("The final result is: " + result.)
