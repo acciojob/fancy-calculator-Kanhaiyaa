e5ded0b071b73634c9b5f7af9955625fd68fb17a
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-13 07:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 
 
 let value1=prompt("enter the 1st no.")
-     let result=parseFloat(value1*2)
+     let result=parseFloat(value1*2)   
 
 let value2=prompt("enter the 2nd no.")
 
-let result +=parseFloat(value2)
-
+ result +=parseFloat(value2)
+  
 alert(`The final result is: ${result}`)
